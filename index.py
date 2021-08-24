9948dcd924e02f6e070d209bb57d5417e3875c37
Jrix <64320121+Jrix-G@users.noreply.github.com>
2021-08-24 02:28:33 +0200

    Updates

--- index.py
+++ index.py
@@ -14,20 +14,22 @@ left2 = ""
 right2 = ""
 up2 = ""
 down2 = ""
+i = 0
 
 x = 10
 y = 10
 width = 10
 height= 10
 
+alala2 = canvas.create_rectangle(x, y, x+width, y+height, fill='red')
 alala = canvas.create_rectangle(x, y, x+width, y+height, fill='red')
 canvas.pack()
 
-
+mange = 0
 
 def Run():
     
-    global left2, up2, down2, right2, x, alala, y
+    global left2, up2, down2, right2, x, alala, y, addcanvas, mange
     window.after(120, Run)
 
     if x > 1390 or x < 0 or y > 690 or y < 0: 
@@ -38,6 +40,9 @@ def Run():
         canvas.delete(alala)
         alala = canvas.create_rectangle(x, y, x+width, y+height, fill='red')
         canvas.pack()
+        if mange < 1:
+            mange+=1
+            addcanvas()
     if left2 == True:
         x-=10
         canvas.delete(alala)
@@ -63,8 +68,6 @@ def Run():
         
     def left(event):
         global left2, up2, down2, right2
-        global x,alala
-        
         right2 = False
         up2 = False
         down2 = False
@@ -83,9 +86,14 @@ def Run():
         left2 = False
         down2 = True
         up2 = False
-    def addcanvas():
-        pass
 
+    def addcanvas():
+        global i, alala2
+        i+=1
+        name = str(i)
+        name = canvas.create_rectangle(x, y, x+width, y+height, fill='green')
+        canvas.pack()
+        canvas.delete(i)
     window.bind("<Right>", right)
     window.bind("<Left>", left)
     window.bind("<Up>", up)
