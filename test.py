bf5c7698c9617c07f97219c05ddf70524121d72e
Jrix <64320121+Jrix-G@users.noreply.github.com>
2021-09-09 09:43:27 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-tab = [[10, 15], [0, 2], [10, 15], [10, 15],[0, 2],[0, 3]]
-
-for i in range(len(tab)):
-    m = 0
-    for n in range(len(tab)):
-        if tab[i] == tab[n]:
-            m += 1
-            if m == 2:
-                print(tab[n])
